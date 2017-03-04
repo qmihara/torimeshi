682ee48218a39bdb2f227153e56311ec8658bf4a
Kyusaku Mihara <qmihara@gmail.com>
2017-03-04 16:35:44 +0900

    linux...

--- Sources/Message.swift
+++ Sources/Message.swift
@@ -9,7 +9,14 @@ struct Message {
         var text = "[\(event.repository.fullName)] \(review.state.emoji) \(review.state.text) by \(review.user.login) on pull request \(review.htmlURL)"
         if let body = event.review.body, !body.isEmpty {
             text += "\n"
-            text += body
+            let carriageReturnRemovedBody = body.replacingOccurrences(of: "\r", with: "")
+            print("carriageReturnRemovedBody:\(carriageReturnRemovedBody)")
+            let splitedBody = carriageReturnRemovedBody.components(separatedBy: "\n")
+            print("splitedBody:\(splitedBody)")
+            let quotedBody = splitedBody.map { "> \($0)" }
+            print("quotedBody:\(quotedBody)")
+            let joinedBody = quotedBody.joined(separator: "\n")
+            text += joinedBody
 //            text += body.replacingOccurrences(of: "\r", with: "").components(separatedBy: "\n").map({ "> \($0)" }).joined(separator: "\n")
         }
         self.text = text
