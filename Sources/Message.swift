b22b64c13e0869dd09ffdcd547a84faea37500d2
Kyusaku Mihara <qmihara@gmail.com>
2017-03-04 16:11:16 +0900

    linux...

--- Sources/Message.swift
+++ Sources/Message.swift
@@ -5,14 +5,8 @@ struct Message {
     var text: String
 
     init(event: PullRequestReviewEvent) {
-        var text = String(
-            format: "[%@] %@ %@ by %@ on pull request %@",
-            event.repository.fullName,
-            event.review.state.emoji,
-            event.review.state.text,
-            event.review.user.login,
-            event.review.htmlURL.absoluteString
-        )
+        let review = event.review
+        var text = "[\(event.repository.fullName)] \(review.state.emoji) \(review.state.text) by \(review.user.login) on pull request \(review.htmlURL)"
         if let body = event.review.body, !body.isEmpty {
             text += "\n"
             text += body.components(separatedBy: "\n").map({ "> \($0)" }).joined(separator: "\n")
