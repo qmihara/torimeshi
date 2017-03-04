3847ba2f2c1480a33bc40974d387dca52c02d24e
Kyusaku Mihara <qmihara@gmail.com>
2017-03-04 16:23:58 +0900

    crashing on heroku...

--- Sources/Message.swift
+++ Sources/Message.swift
@@ -7,10 +7,10 @@ struct Message {
     init(event: PullRequestReviewEvent) {
         let review = event.review
         var text = "[\(event.repository.fullName)] \(review.state.emoji) \(review.state.text) by \(review.user.login) on pull request \(review.htmlURL)"
-        if let body = event.review.body, !body.isEmpty {
-            text += "\n"
-            text += body.replacingOccurrences(of: "\r", with: "").components(separatedBy: "\n").map({ "> \($0)" }).joined(separator: "\n")
-        }
+//        if let body = event.review.body, !body.isEmpty {
+//            text += "\n"
+//            text += body.replacingOccurrences(of: "\r", with: "").components(separatedBy: "\n").map({ "> \($0)" }).joined(separator: "\n")
+//        }
         self.text = text
     }
 }
